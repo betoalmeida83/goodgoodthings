70ca8a8ff5450cc6d58636f0be7cb942241bf4e5
Beto Almeida <roberto.almeida5@fatec.sp.gov.br>
2025-08-19 11:05:40 -0300

    Projeto back-end criado

---
+++ src/main/java/com/goodgoodthings/GoodGoodThingsApplication.java
@@ -0,0 +1,13 @@
+package com.goodgoodthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodGoodThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodGoodThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodgoodthings/GoodGoodThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodgoodthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodGoodThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
